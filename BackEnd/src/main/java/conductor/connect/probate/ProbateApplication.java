9d5a70f744858d78e21af1ff430fa5f43ac0e58a
luiz Alex <luizalex1997@gmail.com>
2020-10-15 11:13:05 -0300

    Start

---
+++ BackEnd/src/main/java/conductor/connect/probate/ProbateApplication.java
@@ -0,0 +1,13 @@
+package conductor.connect.probate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbateApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/conductor/connect/probate/ProbateApplicationTests.java
@@ -0,0 +1,13 @@
+package conductor.connect.probate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
