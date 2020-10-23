9b3a175d46722cbe43f53519bd20bf11bbf2614e
luiz Alex <luizalex1997@gmail.com>
2020-10-23 11:39:47 -0300

    Add Branch to Config Server

--- ConfigServer/src/main/java/conductor/connect/probate/configserver/Application.java
+++ ConfigServer/src/main/java/conductor/connect/probate/configserver/Application.java
@@ -1,10 +1,13 @@
 package conductor.connect.probate.configserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigServer
 public class Application {
 
