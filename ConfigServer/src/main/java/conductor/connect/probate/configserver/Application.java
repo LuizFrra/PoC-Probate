ead3a645d6ca6a38832bac2980c4a6eb5697f720
luiz Alex <luizalex1997@gmail.com>
2020-10-25 10:46:22 -0300

    update database properties

--- ConfigServer/src/main/java/conductor/connect/probate/configserver/Application.java
+++ ConfigServer/src/main/java/conductor/connect/probate/configserver/Application.java
@@ -2,16 +2,18 @@ package conductor.connect.probate.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
 
 @Configuration
 @EnableAutoConfiguration
 @EnableConfigServer
 public class Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(Application.class, args);
 	}
 
