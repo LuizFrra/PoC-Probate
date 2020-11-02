62ae7c552429d4ebec78ec706148731e582f41fa
luiz Alex <luizalex1997@gmail.com>
2020-11-02 13:22:32 -0300

    Add Retry

--- BackEnd/src/main/java/conductor/connect/probate/ProbateApplication.java
+++ BackEnd/src/main/java/conductor/connect/probate/ProbateApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 
 @Slf4j
 @SpringBootApplication
